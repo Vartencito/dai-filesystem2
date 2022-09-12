e21e029c62d6447de91f646d4ac062424a6ffc33
Valen <vatenenberg@gmail.com>
2022-09-11 21:39:01 -0300

    terminada la parte de directorios

--- App.js
+++ App.js
@@ -1,32 +1,73 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, PermissionsAndroid, Button} from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 import * as FileSystem from 'expo-file-system';
 
 export default function App() {
 
-  const directory = FileSystem.documentDirectory
-  console.log(directory)
+  // const directory = FileSystem.documentDirectory;
+  // console.log('directorio',directory)  
 
-  const permissions =  FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
-
-  if (permissions.granted) {
-    const uri = permissions.directoryUri;
-  
-    const files = StorageAccessFramework.readDirectoryAsync(uri);
-    console.log('este es el archivo',files)
-    alert(`Files inside ${uri}:\n\n${JSON.stringify(files)}`);
+  const readAndAccessToDirectory = async () => {
+    const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+    console.log('parte 1, permissions', permissions)
+    if (permissions.granted) {
+      const uri = permissions.directoryUri;
+      const files = await FileSystem.StorageAccessFramework.readDirectoryAsync(uri);
+      alert(`Files inside ${uri}:\n\n${JSON.stringify(files)}`);
+    }
   }
-  
-  useEffect(() => {
-    console.log('holaksjd')
-    // requestMultimediaPermission();
-  },[]);
+
+  const createDirectory = async () => {
+    try {
+      const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      console.log('parte 2, permissions', permissions)
+      if (permissions.granted) {
+        const uri = permissions.directoryUri;
+        await FileSystem.StorageAccessFramework.makeDirectoryAsync(uri, 'prueba');
+        alert(`directory "prueba" created`);
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const deleteDirectory = async () => {
+    try {
+      const permissions = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      console.log('parte 3, permissions', permissions)
+      if (permissions.granted) {
+        const uri = permissions.directoryUri;
+        await FileSystem.StorageAccessFramework.deleteAsync(uri);
+        alert(`directory deleted`);
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text></Text>
+      <Button
+        onPress={readAndAccessToDirectory}
+        title="Read and access to directory"
+        style={styles.button}
+      />
+      <Text></Text>
+      <Button
+        onPress={createDirectory}
+        title="Create directory"
+        style={styles.button}
+      />
+      <Text></Text>
+      <Button
+        onPress={deleteDirectory}
+        title="Delete directory"
+        style={styles.button}
+      />
       <StatusBar style="auto" />
     </View>
   );
